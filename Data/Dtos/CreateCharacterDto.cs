3a67bbd977f8b505637c580ce37d8d94a914ed5a
Willian Medeiros <willianmedeiroslm@gmail.com>
2024-12-19 10:22:42 -0400

    feature: creation of Character Dto

---
+++ Data/Dtos/CreateCharacterDto.cs
@@ -0,0 +1,23 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace characters_API.Data.Dtos
+{
+    public class CreateCharacterDto
+    {
+        [Required(ErrorMessage = "O Nome do personagem é obrigatório")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "O campo race é obrigatório.")]
+        public string Race { get; set; }
+
+        [Required(ErrorMessage = "O campo urlImage é obrigatório.")]
+        public string UrlImage { get; set; }
+
+        [MaxLength(500, ErrorMessage = "A descrição não pode exceder 500 caracteres")]
+        public string Description { get; set; }
+
+        [Required(ErrorMessage = "O campo strength é obrigatório.")]
+        [Range(1, 5, ErrorMessage = "O valor de strength deve ser entre 1 e 5.")]
+        public int Strength { get; set; }
+    }
+}
---
+++ Data/Dtos/ReadCharacterDto.cs
@@ -0,0 +1,19 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace characters_API.Data.Dtos
+
+public class ReadCharacterDto
+{
+
+    public int Id { get; set; }
+    public string Name { get; set; }
+
+    public string Race { get; set; }
+
+    public string UrlImage { get; set; }
+
+    public string Description { get; set; }
+
+    public int Strength { get; set; }
+}
+
